f16b97d88b80a606afbc392e05088f6ed0231ee7
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2024-08-14 12:59:32 +0200

    Added react components, static and dinamic urls, changed css modules to sass, changed font and added images

---
+++ src/components/home/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.scss' /* css modules */
+
+export const Hero = () => {
+  return (
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+
+      <h2>empowering your tomorrow, today!</h2>
+    </section>
+  )
+}
