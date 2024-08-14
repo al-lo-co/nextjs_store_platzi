b46b86f9954ef2fa94927585e9dc6ad4c7e0865a
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2024-08-14 14:45:57 +0200

    Added env variables, integration with shopify and change files from sass to scss

---
+++ src/components/shared/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import styles from './Loader.module.scss';
+
+export const Loader = () => {
+  return (
+    <div className={styles.LoaderWrapper}>
+      <div className={styles.Loader}>
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  )
+};
