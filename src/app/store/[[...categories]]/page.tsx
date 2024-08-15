564bf79dc56963dbe27ac864aaac395337aa1bda
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2024-08-15 12:05:06 +0200

    add shopify consumed endpoint and configuration

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,5 +1,7 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
-import { getProducts } from "app/services/shopify"
+import { getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
+import { title } from "process"
 
 interface CategoryProps {
   params: {
@@ -11,6 +13,7 @@ interface CategoryProps {
 export default async function Category(props: CategoryProps) {
 
   const products = await getProducts()
+  //const collections = await getCollections() pararell call endpoints
 
   const { categories } = props.params
   //throw new Error('Error: boom')
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,7 +2,9 @@ import { env } from "app/config/env"
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
-    'categories': ``
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`
+  },
+  collections: {
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`
   }
 }
