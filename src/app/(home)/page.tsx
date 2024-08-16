cbc9f57427d6bc2a5f624d693d27285f3e8892a4
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2024-08-16 14:08:22 +0200

    SEO fixes about the metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "😁 future world!",
+  description: "welcome for the future world",
+  keywords: ["ecommerce", "future", "world", "technology"]
+}
 
 export default function Home() {
   return (
